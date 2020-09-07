4add3504c6768a3f41ee120f63d8837dc0798a06
Solst1tium <solange.leiva@mayor.cl>
2020-09-07 19:50:59 -0300

    creacion de la vista toys

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -11,12 +12,12 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/toys',
+    name: 'Toys',
+    component: () => import(/* webpackChunkName: "toys" */ '../views/Toys.vue'),
+    meta: {
+      login: true
+    }
   }
 ]
 
---
+++ src/views/Toys.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <v-container>
+      <h1>TOYYYYYYYYYYYYYYYYYYYSSSSSSSSSSSS</h1>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
