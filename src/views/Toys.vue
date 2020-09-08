59701527c3adef45d4a4252d95ba3d0555eefd2c
Solst1tium <solange.leiva@mayor.cl>
2020-09-07 21:57:12 -0300

    ccreacion de tabla con los juguetes existentes

--- src/views/Toys.vue
+++ src/views/Toys.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>
-    <v-container>
-      <h1>TOYYYYYYYYYYYYYYYYYYYSSSSSSSSSSSS</h1>
-    </v-container>
-  </div>
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Nombre</th>
+          <th class="text-left">Precio</th>
+           <th class="text-left">Stock</th>
+           <th class="text-left">Codigo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="toy in toys" :key="toy.id">
+          <td>{{ toy.data.name }}</td>
+          <td>{{ toy.data.price }}</td>
+          <td>{{ toy.data.stock }}</td>
+           <td>{{ toy.data.code }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 <script>
+import {mapState, mapActions} from 'vuex'
 export default {
+  computed: {
+    ...mapState(['toys'])
+  },
+  methods: {
+    ...mapActions(['setToys'])
+  },
+  created() {
+    this.setToys()
+    
+  },
   
 }
 </script>
